226f857c2c346dc8da4be9b563980fa8cde10fbb
henry van dusen <hgv2102@columbia.edu>
2016-03-25 14:14:59 -0400

    test adding index

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<a href='advertisements'>ads</a>
+<a href='books'>books</a>
+<a href='magazines'>magazines</a>
+	<?php// get_template_part( 'loop', 'index' ); ?>
+
+<?php get_footer(); ?>
