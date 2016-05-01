f0c5bee75ef762cb7b1bcced1881ff23c77bc27f
henry van dusen <hgv2102@columbia.edu>
2016-05-01 18:36:01 -0400

    more

--- js/gyroScript.js
+++ js/gyroScript.js
@@ -23,7 +23,8 @@
 	        if(show<7 && show>=0 && show!=angle)
 	        	{
 							console.log($('.book_image:visible'))
-					classes = $('.book_image')[current].classList
+					//classes = $('.book_image')[current].classList
+						 classes = $('.book_image:visible').classList;
 		        	$($('.book_image:visible')[current]).addClass('-position-'+show+'-of-7');
 		        	$($('.book_image:visible')[current]).removeClass(classes[1]);
 		        	angle = show;
