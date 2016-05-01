d13f3531531eb9bf2dce9e95c164ddecbdfdfd48
henry van dusen <hgv2102@columbia.edu>
2016-05-01 19:42:55 -0400

    never

--- js/gyroScript.js
+++ js/gyroScript.js
@@ -25,7 +25,7 @@
 							$('.book').map(function(i,e){
 								//console.log('visible: '+$(e).visible());
 								if($(e).visible()){
-									var image = $(e).find('.book_image');
+									var image = $($(e).find('.book_image'));
 									console.log('found this')
 									console.dir(image);
 									classes = image.classList
