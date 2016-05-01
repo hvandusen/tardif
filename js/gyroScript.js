495c523e6b3a67802b4a3d9828b3ecc697f0d79e
henry van dusen <hgv2102@columbia.edu>
2016-05-01 19:04:05 -0400

    e to i

--- js/gyroScript.js
+++ js/gyroScript.js
@@ -22,8 +22,10 @@
 
 	        if(show<7 && show>=0 && show!=angle)
 	        	{
-							$('.book').map(function(i,e){
+							$('.book').map(function(e,i){
+
 								if($(e).visible()){
+									console.log($(e))
 									console.log($(e))
 									// classes = $('.book_image:visible')[current].classList
 		        			// $($('.book_image:visible')[current]).addClass('-position-'+show+'-of-7');
